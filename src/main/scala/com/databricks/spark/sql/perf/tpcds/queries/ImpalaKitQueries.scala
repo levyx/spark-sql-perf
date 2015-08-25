026815bba7936b33312bd4137e82954ddc34cc7a
Hamid Mirzaei <hmirzai@gmail.com>
2015-08-25 16:28:07 -0700

    change to xenon
    - add xenon specific queries (a subset of whole just for debugging purposes)
    - change parquet codes to xenon codes
    - comment out parquet partitioned table writing
    -

--- src/main/scala/com/databricks/spark/sql/perf/tpcds/queries/ImpalaKitQueries.scala
+++ src/main/scala/com/databricks/spark/sql/perf/tpcds/queries/ImpalaKitQueries.scala
@@ -1472,4 +1472,5 @@ object ImpalaKitQueries {
   val reportingQueries = Seq("q3","q7","q27","q43", "q53", "q89").map(queriesMap)
   val deepAnalyticQueries = Seq("q34", "q46", "q59", "q65",  "q79", "ss_max").map(queriesMap)
   val impalaKitQueries = interactiveQueries ++ reportingQueries ++ deepAnalyticQueries
+  val xenonQueries = Seq("q19").map(queriesMap)
 }
